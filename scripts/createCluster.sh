3c8badd985b63de8673f17427e5ebbfa3b115743
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2022-11-28 16:02:37 -0800

    Create createCluster.sh

---
+++ scripts/createCluster.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+az login
+
+az aks create \
+  --resource-group aks-scaling-demo \
+  --name scaledAKSCluster \
+  --node-count 1 \
+  --enable-cluster-autoscaler \
+  --min-count 1 \
+  --max-count 3 \
+  --cluster-autoscaler-profile scan-interval=30s
