b0d1e846db84a507e18597e719c0ad526a5f7c51
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2023-01-12 17:12:29 -0800

    Update createWindowsCluster.sh

--- scripts/createWindowsCluster.sh
+++ scripts/createWindowsCluster.sh
@@ -13,7 +13,7 @@ az aks create \
   --max-count 3 \
   --os-sku Windows2019 \
   --cluster-autoscaler-profile scan-interval=30s \
-  --windows-admin-password <your admin password> 
+  --windows-admin-password <your admin password> \
   --windows-admin-username <your admin username>
 
 az aks get-credentials --name scaledAKSCluster -g aks-scaling-demo --overwrite
